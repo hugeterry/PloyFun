ff12e7285a0ee82abd6f341b8e0788e37818f388
HugeTerry <xuxt-314@qq.com>
2016-08-14 19:56:13 +0800

    添加view控件，优化界面，增加保存分享功能，整合逻辑

--- app/src/main/java/cn/hugeterry/ployfun/PolyfunKey.java
+++ app/src/main/java/cn/hugeterry/ployfun/PolyfunKey.java
@@ -6,7 +6,8 @@ package cn.hugeterry.ployfun;
  */
 public class PolyfunKey {
     public static final int graMax = 30;//边缘检测的阀值控制，值越大，出来的点越少
-    public static final int pc =800;//最后绘制的点数量控制
-    public static final int initialSize = 2800;//包围三角形的大小
+    public static final int pc = 800;//最后绘制的点数量控制
+    public static final int initialSize = 4000;//包围三角形的大小
     public static int started = 0;
+    public static boolean FIRST = true;
 }
--- app/src/main/java/cn/hugeterry/ployfun/View/MainActivity.java
+++ app/src/main/java/cn/hugeterry/ployfun/View/MainActivity.java
@@ -5,16 +5,25 @@ import android.graphics.Bitmap;
 
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import cn.hugeterry.ployfun.PolyfunKey;
 import cn.hugeterry.ployfun.R;
 import cn.hugeterry.ployfun.core.StartPolyFun;
 import cn.hugeterry.ployfun.utils.GetPhoto;
+import cn.hugeterry.ployfun.utils.SavePhoto;
+import cn.hugeterry.ployfun.utils.ShareUtils;
 
 /**
  * Created by hugeterry(http://hugeterry.cn)
@@ -22,30 +31,91 @@ import cn.hugeterry.ployfun.utils.GetPhoto;
  */
 public class MainActivity extends AppCompatActivity {
     private ImageView iv, seeit;
-    private Button doit;
+    private Toolbar toolbar;
+    private Uri uri;
+    private LinearLayout ll_choose, ll_result;
+    private Button bt_save, bt_share;
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initToolbar();
+        initView();
+
+    }
+
+    private void initView() {
         iv = (ImageView) findViewById(R.id.iv);
-        seeit = (ImageView) findViewById(R.id.seeit);
-        doit = (Button) findViewById(R.id.doit);
-
-        iv.setOnClickListener(new View.OnClickListener() {
+        ll_choose = (LinearLayout) findViewById(R.id.ll_choose);
+        ll_result = (LinearLayout) findViewById(R.id.ll_result);
+        bt_save = (Button) findViewById(R.id.bt_save);
+        bt_share = (Button) findViewById(R.id.bt_share);
+        bt_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("PolyFun", path + "");
+                if (path == null || path.length() == 0) {
+                    path = SavePhoto.writePhoto(iv.getDrawingCache());
+                    SavePhoto.scanPhotos(path, MainActivity.this);
+                    Snackbar.make(iv, "已保存到" + path, Snackbar.LENGTH_LONG).show();
+                } else Snackbar.make(bt_save, "该图片已保存", Snackbar.LENGTH_SHORT).show();
+            }
+
+
+        });
+        bt_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("PolyFun", path + "");
+                if (path == null || path.length() == 0) {
+                    path = SavePhoto.writePhoto(iv.getDrawingCache());
+                    SavePhoto.scanPhotos(path, MainActivity.this);
+                    Snackbar.make(iv, "已保存到" + path, Snackbar.LENGTH_LONG).show();
+                }
+                Log.i("PolyFun", path + "");
+                ShareUtils.shareImage(MainActivity.this, path);
+            }
+        });
+
+    }
+
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_choose:
+                path = null;
+                ll_result.setVisibility(View.INVISIBLE);
+                ll_choose.setVisibility(View.VISIBLE);
                 GetPhoto.gallery(MainActivity.this);
-
-            }
-        });
-        doit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new StartPolyFun(MainActivity.this, iv, seeit);
-            }
-        });
+                break;
+            case R.id.action_done:
+                if (uri != null) {
+                    new StartPolyFun(this, iv, iv);
+                    ll_choose.setVisibility(View.INVISIBLE);
+                    ll_result.setVisibility(View.VISIBLE);
+                } else Snackbar.make(iv, "请先选择图片再制作", Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.action_about_me:
+                break;
+            case R.id.action_about:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -55,27 +125,15 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case 12:
                 if (resultCode == RESULT_OK) {
-                    Uri uri = data.getData();
+                    uri = data.getData();
                     Log.i("uri", uri + "");
-                    // 取得返回的Uri,基本上选择照片的时候返回的是以Uri形式，但是在拍照中有得机子呢Uri是空的，所以要特别注意
-                    if (uri != null) {
-                        Bitmap image;
-                        try {
-                            // 这个方法是根据Uri获取Bitmap图片的静态方法
-                            image = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                            iv.setImageBitmap(image);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    } else {
-                        Bundle extras = data.getExtras();
-                        if (extras != null) {
-                            // 这里是有些拍照后的图片是直接存放到Bundle中的所以我们可以从这里面获取Bitmap图片
-                            Bitmap image = extras.getParcelable("data");
-                            if (image != null) {
-                                iv.setImageBitmap(image);
-                            }
-                        }
+                    Bitmap image;
+                    try {
+                        // 这个方法是根据Uri获取Bitmap图片的静态方法
+                        image = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                        iv.setImageBitmap(image);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
                 break;
