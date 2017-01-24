3bccaf5677dc0132778ca537dc5a11dcb6a5dccc
HugeTerry <xuxt-314@qq.com>
2017-01-25 00:15:07 +0800

    优化结构

--- app/src/main/java/cn/hugeterry/ployfun/utils/GetPhoto.java
+++ app/src/main/java/cn/hugeterry/ployfun/utils/GetPhoto.java
@@ -9,9 +9,6 @@ import android.content.Intent;
  * Date: 16/8/7 19:27
  */
 public class GetPhoto {
-    /*
-     * 从相册获取
-    */
     public static void gallery(Context context) {
         Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         ((Activity) context).startActivityForResult(intent, 12);
